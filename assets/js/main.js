ecf8bdd320042533b704405252a5097788d17e87
lucy <dlucykim@gmail.com>
2021-11-03 18:01:10 +0900

    휴

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
-// $(document).ready(function(){
-//     $('.slider').bxSlider({
-//         auto: true,
-//     });
-// });
-$(document).ready(function(){
-    $('.slider').bxSlider();
-  });
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
