bfe823265a677db330d82c841fe7734c37c2e708
lucy <dlucykim@gmail.com>
2021-11-08 13:02:29 +0900

    점검

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
   loop: true,
-  // autoplay: {
-  //   delay: 2000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
